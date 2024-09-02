9476d9a4c2d41133bc703a118f03ddc9bda05bfb
Vitoria <2411550024@fema.edu.br>
2024-09-02 10:59:11 -0300

    adiciona o projeto ao git

---
+++ assets/javascript/script.js
@@ -0,0 +1,40 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = window.document.getElementById(`txtano`)
+    var res = window.document.querySelector(`div#res`)
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert(`[ERRO] Verifique os dados e tente novamente!`)
+    } else {
+        var fsex = window.document.getElementsByName(`radsex`)
+        var idade = ano - Number(fano.value)
+        var gênero = ``
+        var img = document.createElement(`img`)
+        img.setAttribute(`id`, `foto`)
+        if (fsex[0].checked) {
+            gênero = `Homem`
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute(`src`, `img/homemcrianca.png`)
+            } else if (idade >= 10 && idade < 23) {
+                img.setAttribute(`src`, `img/homemjovem.png`)
+            } else if (idade >= 23 && idade < 50) {
+                img.setAttribute(`src`, `img/homemadulto.png`)
+            } else{
+                img.setAttribute(`src`, `img/homemidoso.png`)
+            }
+        } else if (fsex[1].checked) {
+            gênero = `Mulher`
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute(`src`, `img/mulhercrianca.png`)
+            } else if (idade >= 10 && idade < 21) {
+                img.setAttribute(`src`, `img/mulherjovem.png`)
+            } else if (idade >= 21 && idade < 50) {
+                img.setAttribute(`src`, `img/mulheradulta.png`)
+            } else{
+                img.setAttribute(`src`, `img/mulheridosa.png`)
+            }
+        }
+        res.innerHTML = `${gênero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
